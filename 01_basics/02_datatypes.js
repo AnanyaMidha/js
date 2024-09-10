77769f645e7339e871a0718de67460fad7a1358b
Ananya Midha <126793977+AnanyaMidha@users.noreply.github.com>
2024-09-10 07:32:50 +0000

    datatype

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,12 @@
+"use strict"; //treat all the code as newer version
+// alert(3+3) //we are using node js, not browser
+console.log(3+3); 
+console.log("Ananya")
+let name="Ananya"
+let age=18
+let isLoggedIn=true
+// number
+//bigint
+//boolean 
+//null
+
